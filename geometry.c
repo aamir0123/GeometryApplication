532257931b982af253a33585efd24bd27ea84a0f
Aamir <aamir@abc.com>
2014-04-26 15:21:17 +0530

    Included code for Area Of Square function

--- geometry.c
+++ geometry.c
@@ -2,12 +2,20 @@
 
 int main()
 {
+int a;
+
+a=calAreaOfSquare();
 	// stub for intro message and switch case statements
 	return 0;
 }
 
 int calAreaOfSquare()
 {
+int a;
+printf("Enter Side\n");
+scanf("%d",&a);
+printf("Area is \t",a*a);
+return a;
     // stub for calculating Area of Square
 }
 int calAreaOfTriangle()
